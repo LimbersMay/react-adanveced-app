5d56dd520023178bfd42362d312eaa7fc9071432
limberMay <limbermay1@gmail.com>
2023-04-03 00:50:04 -0600

    Control props alt

--- src/02-component-patterns/hooks/useShoppingCard.ts
+++ src/02-component-patterns/hooks/useShoppingCard.ts
@@ -8,22 +8,7 @@ export const useShoppingCard = () => {
     const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
         setShoppingCart(oldShoppingCart => {
 
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
-
-            if ( Math.max(productInCart.count + count, 0 ) > 0) {
-                productInCart.count += count;
-                return {
-                    ...oldShoppingCart,
-                    [product.id]: productInCart
-                }
-            }
-
-            // Delete the product
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            return rest;
-
-
-            /*if (count === 0) {
+            if (count === 0) {
 
                 const { [product.id]: toDelete, ...rest } = oldShoppingCart;
                 return rest;
@@ -32,7 +17,7 @@ export const useShoppingCard = () => {
             return {
                 ...oldShoppingCart,
                 [product.id]: {...product, count}
-            }*/
+            }
         })
     }
 
